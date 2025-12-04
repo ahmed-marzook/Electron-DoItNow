d1e0d212c55bb86a82e58ac7ce768b55a91034bf
Ahmed Marzook <43218910+ahmed-marzook@users.noreply.github.com>
2025-12-04 18:30:22 +0000

    Change main method to static in DoitnowApplication

--- backend/src/main/java/com/kaizenflow/doitnow/DoitnowApplication.java
+++ backend/src/main/java/com/kaizenflow/doitnow/DoitnowApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DoitnowApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(DoitnowApplication.class, args);
 	}
 
